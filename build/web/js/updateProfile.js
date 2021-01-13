4098e956ec6f16adb0248a6c28119a82e5f74e99
Atef Magdy <atefmagdy12@gmail.com>
2021-01-13 21:23:58 +0200

    Update profile completed

--- build/web/js/updateProfile.js
+++ build/web/js/updateProfile.js
@@ -1,4 +1,6 @@
 $('#bad').hide();
+$('#badName').hide();
+$('#badEmail').hide();
 $(document).ready(function () {
     $('#bad').hide();
     $('#updatePass').submit(function (e) {
@@ -47,5 +49,105 @@ $(document).ready(function () {
                 }
             });
         }
+    });
+});
+
+$(document).ready(function (){
+    $('#badName').hide();
+    $('#updateName').submit(function (e) {
+        var userName = $("#user").val();
+        var newName = $("#newName").val();
+        if (newName.length == 0) {
+            $('#badName').show();
+            $('#badNname').html("<strong>Enter the empty input</strong> ");
+            $("#newName").focus();
+            return false;
+        }
+        else{
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: 'updateName',
+                data: {userName: userName, newName: newName},
+                success: function (response) {
+                    var jsonData = JSON.parse(response);
+                    if (jsonData.success == 0) {
+                        alert("Name changed");
+                        location.href="user.jsp";
+                    } 
+                    else {
+                        $('#badName').show();
+                        $('#badName').html("<strong>Error happen!</strong> ");
+                        $("#newName").focus();
+                        return false;
+                    }
+                }
+            });
+        }
+    });
+});
+
+$(document).ready(function (){
+    $('#badEmail').hide();
+    $('#updateEmail').submit(function (e) {
+        var userName = $("#user").val();
+        var newEmail = $("#newEmail").val();
+        
+        if (newEmail.length == 0) {
+            $('#badEmail').show();
+            $('#badEmail').html("<strong>Enter the empty input</strong> ");
+            $("#newEmail").focus();
+            return false;
+        }
+        else{
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: 'updateEmail',
+                data: {userName: userName, newEmail: newEmail},
+                success: function (response) {
+                    var jsonData = JSON.parse(response);
+                    if (jsonData.success == 0) {
+                        alert("Email changed");
+                        location.href="user.jsp";
+                    } 
+                    else {
+                        $('#badEmail').show();
+                        $('#badEmail').html("<strong>Error happen!</strong> ");
+                        $("#newEmail").focus();
+                        return false;
+                    }
+                }
+            });
+        }
+    });
+});
+
+$(document).ready(function (){
+    $('#badRole').hide();
+    $('#updateRole').submit(function (e) {
+        var userName = $("#user").val();
+        var newRole = $("#newRole").val();
+        
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: 'updateRole',
+            data: {userName: userName, newRole: newRole},
+            success: function (response) {
+                var jsonData = JSON.parse(response);
+                if (jsonData.success == 0) {
+                    alert("Role changed");
+                    location.href="user.jsp";
+                } 
+                else {
+                    $('#badRole').show();
+                    $('#badRole').html("<strong>Error happen!</strong> ");
+                    $("#newRole").focus();
+                    return false;
+                }
+            }
+        });
+        
     });
 });
---
+++ src/java/updateRole.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Atef Magdy
+ */
+@WebServlet(urlPatterns = {"/updateRole"})
+public class updateRole extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            //userName: userName, newEmail: newEmail
+            Connection Con = null;
+            PreparedStatement stmt = null;
+            String query = null;
+            String userName = request.getParameter("userName");
+            String newRole = request.getParameter("newRole");
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                String url = "jdbc:mysql://localhost:3306/staffmanager";
+                String user = "root";
+                String Password = "hema@1234";
+                Con = DriverManager.getConnection(url, user, Password);
+                query = "UPDATE user SET role=? where userName=?;";
+                stmt = Con.prepareStatement(query);
+                stmt.setString(1, newRole);
+                stmt.setString(2, userName);
+                int counter=0;
+                counter = stmt.executeUpdate();
+                if (counter == 1) {
+                    request.getSession().setAttribute("role", newRole);
+                    out.print("{\"success\": 0}");
+                } else {
+                    out.print("{\"success\": 4}");
+                }
+
+            } catch (ClassNotFoundException | SQLException ex) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                ex.printStackTrace(pw);
+                String stackTrace = sw.toString();
+                String replace = stackTrace.replace(System.getProperty("line.separator"), "<br/>\n");
+                out.println(replace);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
