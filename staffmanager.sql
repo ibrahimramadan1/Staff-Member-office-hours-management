133d13a99b878395ce810d3ec74ac40ce3532509
ibrahimramadan1 <ibrahemramadan130@gmail.com>
2021-01-15 17:59:45 +0200

    new features added

--- staffmanager.sql
+++ staffmanager.sql
@@ -29,7 +29,7 @@ CREATE TABLE `message` (
   `content` varchar(200) DEFAULT NULL,
   `mDate` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,8 +38,59 @@ CREATE TABLE `message` (
 
 LOCK TABLES `message` WRITE;
 /*!40000 ALTER TABLE `message` DISABLE KEYS */;
-INSERT INTO `message` VALUES (1,'20170002','superhema','hi','2021-01-12 13:24:15'),(2,'superhema','20170002','hi','2021-01-12 13:24:31'),(3,'superhema','20170002','aloha','2021-01-12 18:48:30'),(4,'20170002','superhema','aloha','2021-01-12 18:51:21'),(5,'20170002','superhema','try new feature','2021-01-12 18:58:57'),(6,'20170002','superhema','again','2021-01-12 18:59:38'),(7,'superhema','20170002','okay','2021-01-12 19:01:54'),(8,'superhema','20170002','hi','2021-01-12 19:07:13'),(9,'superhema','20170002','hi again','2021-01-12 19:07:23'),(10,'superhema','20170002','hi 3','2021-01-12 19:08:01'),(11,'superhema','20170002','aloha','2021-01-12 19:11:17'),(12,'superhema','20170002','aloha','2021-01-12 19:11:46'),(13,'superhema','20170002','just trying','2021-01-12 19:11:56'),(14,'superhema','20170002','trying again','2021-01-12 19:12:08'),(15,'superhema','20170002','and again','2021-01-12 19:12:19'),(16,'superhema','20170002','asmdkmdas','2021-01-12 19:12:24'),(17,'superhema','20170002','aflsckasdok,adsolkads','2021-01-12 19:12:29'),(18,'superhema','20170002','asdfas','2021-01-12 19:14:12'),(19,'superhema','20170002','everything is working now','2021-01-12 19:14:29'),(20,'superhema','20170002','well done bro','2021-01-12 19:24:48'),(21,'superhema','20170002','hi from 3atef','2021-01-13 15:32:02'),(22,'20170002','superhema','hi from hema','2021-01-13 15:33:42');
+INSERT INTO `message` VALUES (1,'20170002','superhema','hi','2021-01-12 13:24:15'),(2,'superhema','20170002','hi','2021-01-12 13:24:31'),(3,'superhema','20170002','aloha','2021-01-12 18:48:30'),(4,'20170002','superhema','aloha','2021-01-12 18:51:21'),(5,'20170002','superhema','try new feature','2021-01-12 18:58:57'),(6,'20170002','superhema','again','2021-01-12 18:59:38'),(7,'superhema','20170002','okay','2021-01-12 19:01:54'),(8,'superhema','20170002','hi','2021-01-12 19:07:13'),(9,'superhema','20170002','hi again','2021-01-12 19:07:23'),(10,'superhema','20170002','hi 3','2021-01-12 19:08:01'),(11,'superhema','20170002','aloha','2021-01-12 19:11:17'),(12,'superhema','20170002','aloha','2021-01-12 19:11:46'),(13,'superhema','20170002','just trying','2021-01-12 19:11:56'),(14,'superhema','20170002','trying again','2021-01-12 19:12:08'),(15,'superhema','20170002','and again','2021-01-12 19:12:19'),(16,'superhema','20170002','asmdkmdas','2021-01-12 19:12:24'),(17,'superhema','20170002','aflsckasdok,adsolkads','2021-01-12 19:12:29'),(18,'superhema','20170002','asdfas','2021-01-12 19:14:12'),(19,'superhema','20170002','everything is working now','2021-01-12 19:14:29'),(20,'superhema','20170002','well done bro','2021-01-12 19:24:48'),(21,'superhema','20170002','hi from 3atef','2021-01-13 15:32:02'),(22,'20170002','superhema','hi from hema','2021-01-13 15:33:42'),(23,'superhema','mans','IS345 : Internet Apps :try feature','2021-01-13 17:50:11'),(24,'superhema',NULL,'IS345 : Internet Apps :hi ','2021-01-13 17:53:27'),(25,'superhema',NULL,'IS345 : Internet Apps :hi ','2021-01-13 17:53:27'),(26,'superhema','mans','IS345 : Internet Apps :alo','2021-01-13 17:57:09');
 /*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `officehrs`
+--
+
+DROP TABLE IF EXISTS `officehrs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `officehrs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `oh_date` date DEFAULT NULL,
+  `oh_from` varchar(7) DEFAULT NULL,
+  `oh_to` varchar(7) DEFAULT NULL,
+  `userName` varchar(50) DEFAULT NULL,
+  `type` varchar(8) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `officehrs`
+--
+
+LOCK TABLES `officehrs` WRITE;
+/*!40000 ALTER TABLE `officehrs` DISABLE KEYS */;
+INSERT INTO `officehrs` VALUES (4,'2021-10-10','1:00am','2:00am','superhema','offline','new building');
+/*!40000 ALTER TABLE `officehrs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `id` int DEFAULT NULL,
+  `userName` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -89,7 +140,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('20170002','ibrahim','ibrahemramadan130@gmail.co','be52e17a','ST'),('mans','Ahmed Sayed','ahmedmans@gmail.com','mans','DR'),('superhema','Ibrahim Ramadan','ibrahemramadan130@gmail.com','hema','TA');
+INSERT INTO `user` VALUES ('20170002','ibrahim','ibrahemramadan130@gmail.co','hema','ST'),('mans','Ahmed Sayed','ahmedmans@gmail.com','mans','DR'),('superhema','Ibrahim Ramadan','ibrahemramadan130@gmail.com','hema','TA');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,4 +176,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-13 18:17:47
+-- Dump completed on 2021-01-15 17:57:05
